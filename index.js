027ed57200b6864971e25710d695211aff1e0005
Gergo ERDI <gergo@erdi.hu>
2022-06-11 19:32:48 +0800

    Use MOS6502 core from external library

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function CB_main($0) {
-    let cb = n => w => { console.log(n); return 10 * n};
-    let x1 = cbtest1(cb)    ;
-    let x2 = cbtest2(cb)($0);
-
-    console.log("CB_main", x1, x2);
-};
-
 function timeIt() {
     let before = +new Date();
     let cnt = initialize(resolveFile);
